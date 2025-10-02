7e4a6816cbb91c1f246ca4e42817186670ad5668
Virus2hell <atharvanarvekar269@gmail.com>
2025-10-02 18:28:51 +0530

    updated-app-tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,12 +4,16 @@ import HeroSection from './components/HeroSection';
 import AboutUs from './components/AboutUs';
 //import StatsSection from './components/StatsSection';
 import Footer from './components/Footer';
+import ImageSearch from './components/ImageSearch';
+import EmailChecker from './components/EmailChecker';
 
 function App() {
   return (
     <div className="min-h-screen">
       <Header />
       <HeroSection />
+      <ImageSearch />
+      <EmailChecker />
       <AboutUs />
       {/* <StatsSection /> */}
       <Footer />
