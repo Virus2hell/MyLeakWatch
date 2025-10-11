b81ef2674f9b9885a7495fb31e7a7aa263b3ba06
Virus2hell <atharvanarvekar269@gmail.com>
2025-10-11 23:01:59 +0530

    updated App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
@@ -6,26 +7,39 @@ import ImageSearch from './components/ImageSearch';
 import EmailChecker from './components/EmailChecker';
 import Contact from './components/Contact';
 import Docs from './components/Docs';
+import VaultPage from './components/PasswordManager/VaultPage';
 
 type Page = 'home' | 'docs';
 
 function App() {
   const [page, setPage] = useState<Page>('home');
 
+  const HomeScreen = () => (
+    <>
+      <EmailChecker />
+      <ImageSearch />
+      <AboutUs />
+      <Contact />
+    </>
+  );
+
   return (
-    <div className="min-h-screen">
-      <Header onNavigate={setPage} currentPage={page} />
-      {page === 'home' && (
-        <>
-          <EmailChecker />
-          <ImageSearch />
-          <AboutUs />
-          <Contact />
-        </>
-      )}
-      {page === 'docs' && <Docs />}
-      <Footer onNavigate={setPage} currentPage={page} />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <Header onNavigate={setPage} currentPage={page} />
+
+        <Routes>
+          {/* Keep SPA-style sections controlled by page state */}
+          <Route path="/" element={page === 'home' ? <HomeScreen /> : <Docs />} />
+          {/* Explicit docs path if someone navigates directly */}
+          <Route path="/docs" element={<Docs />} />
+          {/* Password Manager route */}
+          <Route path="/vault" element={<VaultPage />} />
+        </Routes>
+
+        <Footer onNavigate={setPage} currentPage={page} />
+      </div>
+    </BrowserRouter>
   );
 }
 
